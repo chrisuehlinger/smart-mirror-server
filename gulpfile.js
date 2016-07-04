ad20a97df09d67dfa93ca3c5c836e1f56427c46f
Chris Uehlinger <chris.uehlinger@gmail.com>
2016-07-03 20:42:28 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  plumber = require('gulp-plumber'),
+  livereload = require('gulp-livereload'),
+  sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./public/css/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('./public/css'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./public/css/*.scss', ['sass']);
+});
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'bin/www',
+    ext: 'js ejs coffee',
+  }).on('restart', function () {
+    setTimeout(function () {
+      livereload.changed(__dirname);
+    }, 500);
+  });
+});
+
+gulp.task('default', [
+  'sass',
+  'develop',
+  'watch'
+]);
