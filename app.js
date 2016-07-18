ff4193c6a1fb7a493636ffcfd1fe6e3c3110cae0
Chris Uehlinger <chris.uehlinger@gmail.com>
2016-07-18 18:45:31 -0400

    Rename api to alexa

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/user');
-var api = require('./routes/api');
+var alexa = require('./routes/alexa');
 
 
 var app = express();
@@ -35,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/api', api);
+app.use('/alexa', alexa);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
