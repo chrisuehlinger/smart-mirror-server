158b32deb5dac9f213bfb9cb01fffe57cab5f8f6
Chris Uehlinger <chris.uehlinger@gmail.com>
2016-07-18 23:04:24 -0400

    Strip newlines back out, leave periods in.

--- routes/intents/hacker-news.js
+++ routes/intents/hacker-news.js
@@ -4,10 +4,10 @@ var html2text = require("html-to-text");
 module.exports = function hackerNews(req, res) {
   //TODO: Make all of this async, this could probably perform much better.
 
-  var summary = 'Today\'s Top Hacker News Stories: ';
+  var summary = '<s>Today\'s Top Hacker News Stories:</s>';
   api.getTopStories().slice(0,10).forEach(function(id, i){ 
     var story = api.getItem(id);
-    summary += 'Number ' + (i+1) + ': ' + story.title + '.\n';
+    summary += '<s>Number ' + (i+1) + ': ' + story.title + '</s>';
     
     if(story.kids && story.kids.length) {
       var topComment = api.getItem(story.kids[0]);
@@ -17,8 +17,8 @@ module.exports = function hackerNews(req, res) {
         topCommentText = topCommentText.replace(/(<p>)?<pre>.*<\/pre>/, '');
         topCommentText = topCommentText.slice(0,topCommentText.indexOf('<p>', topCommentText.indexOf('<p>')+1));
         topCommentText = html2text.fromString(topCommentText);
-        // topCommentText = topCommentText.replace(/\n/g, ' ');
-        summary += 'Top Comment: ' + topCommentText + '.\n';
+        topCommentText = topCommentText.replace(/\n+/g, ' ');
+        summary += '<s>Top Comment:</s>' + topCommentText + ' ';
       }
     }
   });
