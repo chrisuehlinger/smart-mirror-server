5934d42eb955fc1f7f67b03af6441f4680fa2f72
Chris Uehlinger <chris.uehlinger@gmail.com>
2016-07-26 13:47:05 -0400

    Fix Alexa intent parsing error

--- routes/alexa.js
+++ routes/alexa.js
@@ -5,28 +5,27 @@ var weather = require('./intents/weather');
 var hackerNews = require('./intents/hacker-news');
 
 router.post('/', function(req, res) {
-  var intentDictionary = {
-    Weather: {
-      outputSpeechType: 'PlainText',
-      cardTitle: 'Today\'s Weather',
-      fn: weather
-    },
-    HackerNewsTopStories: {
-      outputSpeechType: 'SSML',
-      cardTitle: 'Today\'s Top Hacker News Stories',
-      fn: hackerNews.topStories
-    },
-    NoIntent: {
-      outputSpeechType: 'PlainText',
-      cardTitle: intent.name || 'No Intent',
-      fn: function(callback){
-        callback('Intent "' + intent.name +'" not recognized');
-      }
-    }
-  };
-
 
   var intent = req.body.request.intent,
+      intentDictionary = {
+        Weather: {
+          outputSpeechType: 'PlainText',
+          cardTitle: 'Today\'s Weather',
+          fn: weather
+        },
+        HackerNewsTopStories: {
+          outputSpeechType: 'SSML',
+          cardTitle: 'Today\'s Top Hacker News Stories',
+          fn: hackerNews.topStories
+        },
+        NoIntent: {
+          outputSpeechType: 'PlainText',
+          cardTitle: intent.name || 'No Intent',
+          fn: function(callback){
+            callback('Intent "' + intent.name +'" not recognized');
+          }
+        }
+      },
       intentKey = intentDictionary[intent.name] ? intent.name : 'NoIntent';
 
   intentDictionary[intentKey].fn(function(summary){
